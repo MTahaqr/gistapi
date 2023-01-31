ff8d1489ecab63cd6a623efe8b09b4c5304f97ec
MTahaqr <tahaqr28@outlook.com>
2023-01-31 14:38:05 +0500

    refactor: renamed variables and adde comments

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+// Packages
 import { RotateSpinner } from 'react-spinners-kit';
 import styled from 'styled-components';
 
 interface LoaderProp {
   color?: string;
 }
-const Loader: React.FC<LoaderProp> = ({ color = '#24292e' }) => (
-  <StyledContainer>
-    <RotateSpinner color={color} />
-  </StyledContainer>
-);
+const Loader: React.FC<LoaderProp> = ({ color = '#24292e' }) => {
+  /**
+   * @Render
+   */
+  return (
+    <StyledContainer>
+      <RotateSpinner color={color} />
+    </StyledContainer>
+  );
+};
 
 const StyledContainer = styled.div`
   display: flex;
