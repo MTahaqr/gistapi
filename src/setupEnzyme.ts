bf6d28011928faf4ca8c134b82e94a25dc4ba97c
MTahaqr <tahaqr28@outlook.com>
2023-01-31 14:41:58 +0500

    test: added unit tests

---
+++ src/setupEnzyme.ts
@@ -0,0 +1,5 @@
+import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new EnzymeAdapter() });
